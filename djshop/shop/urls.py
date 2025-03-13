f8cce31c653ff1de5962782cc1637b6d053f9030
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-03-13 19:50:57 +0100

    Add single product page.

--- djshop/shop/urls.py
+++ djshop/shop/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.shop, name="shop"),
+    path('product/<slug:slug>', views.product_info, name="product-info"),
 ] 
--- djshop/shop/views.py
+++ djshop/shop/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
 from .models import Category, Product
 
 
@@ -17,3 +19,14 @@ def categories(request):
     all_categories = Category.objects.all()
 
     return {"all_categories": all_categories}
+
+
+def product_info(request, slug):
+
+    product = get_object_or_404(Product, slug=slug)
+
+    context = {
+        "product": product
+    }
+
+    return render(request, "shop/product-info.html", context)
