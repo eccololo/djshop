c510da3a1595ab3de746307a05cb5be91fa5d6dd
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-04-16 19:14:48 +0200

    Add checkout template.

--- djshop/payment/views.py
+++ djshop/payment/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 
+def checkout(request):
+
+    return render(request, "payment/checkout.html")
+
+
 def payment_success(request):
 
     return render(request, "payment/payment-success.html")
