080b7017cfbf71d043f1eb735ed18e918053e106
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-04-17 21:05:48 +0200

    Add ajax to checkout template. Add complete order view function.

--- djshop/payment/urls.py
+++ djshop/payment/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("checkout/", views.checkout, name="checkout"),
+    path("complete-order/", views.complete_order, name="complete-order"),
     path("payment-success/", views.payment_success, name="payment-success"),
     path("payment-failed/", views.payment_failed, name="payment-failed"),
 ] 
--- djshop/payment/views.py
+++ djshop/payment/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
-from .models import ShippingAddress
+from .models import ShippingAddress, Order, OrderItem
+
+from cart.cart import Cart
 
 
 def checkout(request):
@@ -35,3 +37,30 @@ def payment_success(request):
 def payment_failed(request):
 
     return render(request, "payment/payment-failed.html")
+
+
+def complete_order(request):
+
+    if request.POST.get("action") == "post":
+
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+
+        address1 = request.POST.get("address1")
+        address2 = request.POST.get("address2")
+        city = request.POST.get("city")
+
+        state = request.POST.get("state")
+        zipcode = request.POST.get("zipcode")
+
+        # All-in-one shipping address
+        shipping_address = (address1 + "\n" + address2 + "\n" + 
+                            city + "\n" + state + "\n" + zipcode)
+        
+        # Shopping cart information
+        cart = Cart(request)
+
+        # Get total cost of items
+        total_cost = cart.get_total()
+
+        
